ad36f9dfff6c7e554ffd35b63dd9e838207a7858
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-12-13 02:25:40 +0530

    resource-groups

---
+++ resource-groups/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+    type = string
+    description = "resource group name"
+}
+
+variable "location" {
+    type = string
+    description = "location / region"
+}
+
+variable "environment" {
+    type = string
+    description = "environment"
+}
