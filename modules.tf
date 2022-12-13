e68c647ed9396a8a7f76fa594c826cbe53979b16
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-12-13 14:56:08 +0530

    Modules

--- modules.tf
+++ modules.tf
@@ -15,3 +15,15 @@ module "mssql_server" {
     environment = var.env
 }
 
+module "virtual_network" {
+  source = "./Virtual-networks"
+  resource_group_name = module.resource_group.resource_group_name
+  resource_group_location = var.location
+  virtual_network_name = "${var.project}-vnet-1"
+  subnet1_name = "${var.project}-subnet-1"
+  public_ip_name = "${var.project}-public-ip-1"
+  virtual_network_gateway_name = "${var.project}-vnet-gateway-1"
+  ip_configuration_name = "${var.project}-ip-configuration-1"
+  environment = var.env
+}
+
