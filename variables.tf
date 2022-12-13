6fb2da1a06f09bb7e1d567387d6ceee9e8190012
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-12-13 14:55:29 +0530

    Variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+    type = string
+    description = "Specific project the resource belongs to "
+}
+
+variable "env" {
+    type = string
+    description = "Environment the resource belongs to "
+}
+
+variable "location" {
+    type = string
+    description = "Location the resource belongs to "
+}
